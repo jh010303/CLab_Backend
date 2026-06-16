7d81c98bf42919677beb5a6774a5935c52527fae
jh010303 <jh010303@naver.com>
2026-06-16 13:48:34 +0900

    refactor: @Data 제거, 컬럼 이름 변경

--- clab/src/main/java/com/clab/personaAnalysis/dto/PersonaAnalysisDto.java
+++ clab/src/main/java/com/clab/personaAnalysis/dto/PersonaAnalysisDto.java
@@ -2,14 +2,14 @@ package com.clab.personaAnalysis.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Getter
 @AllArgsConstructor
 public class PersonaAnalysisDto {
 	private Integer id;
-	private Integer chattingId;
+	private Integer chatId;
 	private Integer participantId;
 	private Integer personaId;
 	private String analysisSummary;
