496df2cd1172d60cf2e63a2431be27c189ad1da1
김준형 <jh010303@naver.com>
2026-06-22 17:45:21 +0900

    refactor: 페르소나+대화 참여자 DTO

---
+++ clab/src/main/java/com/clab/participant/dto/ParticipantPersonaDto.java
@@ -0,0 +1,30 @@
+package com.clab.participant.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ParticipantPersonaDto {
+	private Integer id;
+	// 대화 참여자
+	private Integer chatId;
+	private String chatName;
+	private Integer count;
+	private Integer averageReplyTime;
+	private Integer chatLength;
+	
+	// 페르소나 분석
+	private Integer personaAnalysisId;
+	private String analysisSummary;
+	private String speechStyle;
+	private Integer tetoScore;
+	
+	// 인물상
+	private Integer personaId;
+	private String personaName;
+	private String description;
+	private String image;
+}
